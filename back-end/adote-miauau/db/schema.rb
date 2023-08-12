dbf1f49488e71f7d0c0e9d31b5e687791dd4e819
rhebeca abreu <rhebecaabreu@gmail.com>
2023-08-12 19:47:38 -0300

    Add category model

--- back-end/adote-miauau/db/schema.rb
+++ back-end/adote-miauau/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_12_223923) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_12_224407) do
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "state"
     t.string "city"
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_223923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable"
+  end
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
