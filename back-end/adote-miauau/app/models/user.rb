7f888f851337b8231dd148011c46fcec9d5c98cb
rhebeca abreu <rhebecaabreu@gmail.com>
2023-08-12 19:40:50 -0300

    Add enumerize gem and enums to user

--- back-end/adote-miauau/app/models/user.rb
+++ back-end/adote-miauau/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  extend Enumerize
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enumerize :type, in: [:protector, :ong], default: :protector
+  enumerize :permissions, in: [:common, :admin], default: :common
+
 end
+
