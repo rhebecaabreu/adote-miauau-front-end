a3a7d5ddac75c53ccda24ae4122d68aa6bec5250
rhebeca abreu <rhebecaabreu@gmail.com>
2023-08-13 20:01:46 -0300

    Fix api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.adotemiauau.com.br/api",
+  baseURL: "https://api.adotemiauau.com.br/api",
 });
 
 api.defaults.headers.common["Content-Type"] = "application/json";
